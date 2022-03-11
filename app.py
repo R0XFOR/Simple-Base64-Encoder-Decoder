40e40521698fed56f0e311c6849516a5be94cc7c
Андрей <90136123+AndreyTsivis@users.noreply.github.com>
2022-03-11 21:08:31 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import base64
-import sys
-import os
+from base64 import b64encode, b64decode
+from sys import exit
+from os import system
 
 def mode():
 	mode=str(input('(D)ecrypt or (E)ncrypt, or exit: '))
@@ -9,11 +9,11 @@ def mode():
 	if mode=='D':
 		decrypt()
 	if mode=='exit':
-		sys.exit()
+		exit()
 
 def encrypt():
     str_msg=input('Enter message: ').encode('utf-8')
-    base64msg_b=base64.b64encode(str_msg)
+    base64msg_b=b64encode(str_msg)
     base64msg=base64msg_b.decode('utf-8')
     print(base64msg)
     mode()
@@ -21,9 +21,9 @@ def encrypt():
 	
 def decrypt():
     base_message=input('Enter message: ').encode('utf-8')
-    base_str=base64.b64decode(base_message).decode('utf-8')
+    base_str=b64decode(base_message).decode('utf-8')
     print(base_str)
     mode()
 
-os.system('cls||clear')
+system('cls||clear')
 mode()
